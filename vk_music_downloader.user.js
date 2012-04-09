c8570427ea69df0e3e053cec8ac2ead94f95bd43
Alex Antonyuk <alex@antonyuk.me>
2012-04-09 14:02:52 +0300

    refactoring

--- vk_music_downloader.user.js
+++ vk_music_downloader.user.js
@@ -1,14 +1,15 @@
 // ==UserScript==
-// @name			VK Music Downloader v1.0.2
+// @name			VK Music Downloader v1.0.3
 // @namespace		name.x-code::vkontakte::music::download
-// @date			2011.06.18
+// @date			2012.04.09
 // @description		Download Music From VK
 // @author			.DeV!L
 // @include			http://vkontakte.ru*
 // @include			http://vk.com*
 // ==/UserScript==
 
-var icon_download = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJ\
+var icon_download = 'data:image/png;base64,\
+iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAGXRFWHRTb2Z0d2FyZQBBZG9iZSBJ\
 bWFnZVJlYWR5ccllPAAAAhBJREFUeNqkk79v00AUx78X26GJkzQqP4qKqLoQMWMhkBASLOyIAYQy\
 wVBGJv6OrFX5sUSoYoCpFagSVKpgac1SFmABofJDpE3t1Mn57LN5F+LEDoWFk55P9967z33P7x2L\
 4xj/M3T1ufTwR8bJGFugyUq5bDpoPp2zdmt6BIikHAdb16/MWEEQQ9cZnqx+/beCMAz/CHiehOuG\
@@ -22,64 +23,58 @@ wavBvjaZehg8OTgN0MhKZKqT2F9av0fWUYVLAL8EGAB/dRts9he48AAAAABJRU5ErkJggg==';
 
 function add_link(to, url, title){
 	var wrap = document.createElement("div");
-	wrap.setAttribute('class', 'fl_r');
-	wrap.setAttribute('style', 'padding-left: 2px');
+		wrap.setAttribute('class', 'fl_r');
+		wrap.setAttribute('style', 'padding-left: 2px');
+	
+	var dl_img = document.createElement('img');
+		dl_img.alt = title;
+		dl_img.src = icon_download;
 	
 	var dl_link = document.createElement("a");
-	var dl_img = document.createElement('img');
-	
-	dl_img.alt = title;
-	dl_img.src = icon_download;
-	
-	dl_link.href = url;
-	dl_link.appendChild(dl_img);
-	
-	dl_link.setAttribute("title", title);
-	dl_link.setAttribute("alt", title);
+		dl_link.href = url;
+		dl_link.appendChild(dl_img);
+		dl_link.setAttribute("title", title);
+		dl_link.setAttribute("alt", title);
 	
 	wrap.appendChild(dl_link)
 	var status = to.getAttribute('_done');
 	if (!status) {
 		to.appendChild(wrap);
-		to.setAttribute('_done', true);		
-	};
+		to.setAttribute('_done', true);
+	}
 }
 
-function get_info(id){
-	var obj = document.getElementById('audio_info' + id);
-	if (obj) {
-		var val = obj.getAttribute('value').split(',');
-		$url = val[0];
-		$length = val[1];
-		var next = obj.parentNode.nextSibling.nextSibling;
-		var ttl = document.getElementById('title' + id);
-		if (!ttl) {
-			var ttl = document.getElementById('audio' + id).getElementsByClassName('title')[0];
-		};
-		$title = ttl.innerHTML.replace(/<[^>]+>/g,"").replace('amp;', '');
-		var artist = next.getElementsByTagName('b')[0];
-		$artist = artist.innerHTML.replace(/<[^>]+>/g,"").replace('amp;', '');
-		var $dur = next.getElementsByClassName('duration')[0];
-		return {
-			'target': $dur,
-			'artist': $artist,
-			'title' : $title,
-			'url'   : $url,
-			'length': $length
-		};
-	}else{
-		return false;
-	}
+function get_info(obj){
+	var val = obj.getAttribute('value').split(','),
+		url = val[0],
+		length = val[1],
+		next_node = obj.parentNode.nextSibling.nextSibling,
+		title_node = document.getElementById('title' + id);
+	if (!title_node) {
+		var title_node = document.getElementById('audio' + id).getElementsByClassName('title')[0];
+	};
+	var title = title_node.innerHTML.replace(/<[^>]+>/g,"").replace('amp;', ''),
+		artist_node = next_node.getElementsByTagName('b')[0],
+		artist = artist_node.innerHTML.replace(/<[^>]+>/g,"").replace('amp;', '');
+	var dur = next_node.getElementsByClassName('duration')[0];
+	return {
+		'target': dur,
+		'artist': artist,
+		'title' : title,
+		'url'   : url,
+		'length': length
+	};
 }
 
 function process(node){
 	_id = /audio(-?[0-9_]+)/.exec(node.id);
 	if (_id) {
-		var info = get_info(_id[1]);
-		if (info) {
+		var audio_info_node = document.getElementById('audio_info' + _id[1]);
+		if (audio_info_node) {
+			var info = get_info(audio_info_node);
 			add_link(info.target, info.url, info.artist + ' - ' + info.title);
-		};
-	};
+		}
+	}
 }
 
 function find_audios(){
@@ -90,7 +85,7 @@ function find_audios(){
 			audio = audios[ind];
 			process(audio);
 		}
-	};
+	}
 }
 
 function onChange(e){
@@ -102,12 +97,12 @@ function onChange(e){
 					audio = audios[ind];
 					process(audio);
 				}
-			};
+			}
 		}
 		
 		if (/class\="[a-z_ ]*\brepeat_wrap\b[a-z_ ]*?"/.test(e.target.innerHTML)) {
 			process(e.target);
-		};
+		}
 	}
 }
 
